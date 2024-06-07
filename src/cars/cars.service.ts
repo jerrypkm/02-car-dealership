1174bac53a83b66a2c6e84d9c6eed6eed81263f3
Gerardo Chapa <gerardochapa1000@gmail.com>
2024-06-06 21:49:31 -0600

    Feat:First steps nest CRUD

---
+++ src/cars/cars.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+@Injectable()
+export class CarsService {
+  private cars = [
+    {
+      id: 1,
+      brand: 'Toyota',
+      model: 'Corolla'
+    },
+    {
+      id: 2,
+      brand: 'Ford',
+      model: 'Focus'
+    },
+    {
+      id: 3,
+      brande: 'Honda',
+      model: 'Civic'
+    }
+  ]
+
+
+  findAll(){
+    return this.cars
+  }
+
+  findOneById(id: number){
+    const car = this.cars.find(car => car.id === id);
+
+    if(!car)
+      throw new NotFoundException(`car with ${id} not found`);
+
+    return car
+  }
+}
